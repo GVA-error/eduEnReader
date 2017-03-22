f3481e379f1aa3959ca39ea71ba0dac663aff346
gva-error <vsv10000ego@gmail.com>
2017-03-22 17:13:06 +0300

    Добавил шрифты , стили и селлектор tourch

--- source/main.cpp
+++ source/main.cpp
@@ -17,10 +17,27 @@ int main(int argc, char *argv[])
 
 #ifdef QT_WIDGETS_LIB
     QApplication app(argc, argv);
+    QApplication::setApplicationName("eduEnReader");
 #else
     QGuiApplication app(argc, argv);
+    QGuiApplication::setApplicationName("eduEnReader");
 #endif
+
+    QFontDatabase fontDatabase;
+    if (fontDatabase.addApplicationFont(":/fonts/fontello.ttf") == -1)
+        qWarning() << "Failed to load fontello.ttf";
+
+    QStringList selectors;
+#ifdef QT_EXTRA_FILE_SELECTOR
+    selectors += QT_EXTRA_FILE_SELECTOR;
+#else
+    if (app.arguments().contains("-touch"))
+        selectors += "touch";
+#endif
+
     QQmlApplicationEngine engine;
+    QQmlFileSelector::get(&engine)->setExtraSelectors(selectors);
+
     QUrl appPath(QString("%1").arg(app.applicationDirPath()));
     engine.rootContext()->setContextProperty("appPath", appPath);
     QtWebEngine::initialize();
@@ -28,6 +45,9 @@ int main(int argc, char *argv[])
     qmlRegisterType<TextStore>("TextStoreModul", 1, 0, "TextStore");
     qmlRegisterType<SoundStore>("SoundStoreModul", 1, 1, "SoundStore");
     engine.load(QUrl(QStringLiteral("qrc:/main_Read.qml")));
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
